9230bd2a828699a4ae665f7528f99dde6dffa202
conghaonet <hao.cong@app2m.com>
2022-12-01 21:11:49 +0800

    fix hit_test_behavior_page.dart

--- lib/src/page/hit_test_behavior_page.dart
+++ lib/src/page/hit_test_behavior_page.dart
@@ -9,8 +9,14 @@ class HitTestBehaviorPage extends StatefulWidget {
 }
 
 class _HitTestBehaviorPageState extends State<HitTestBehaviorPage> {
-  int _redCount = 0;
-  int _blueCount = 0;
+  int _blue1Count = 0;
+  int _red1Count = 0;
+
+  int _blue2Count = 0;
+  int _red2Count = 0;
+
+  int _blue3Count = 0;
+  int _red3Count = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -18,68 +24,133 @@ class _HitTestBehaviorPageState extends State<HitTestBehaviorPage> {
       appBar: AppBar(
         title: const Text('HitTestBehavior'),
       ),
-      body: Container(
-        width: double.infinity,
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Text('请分别触摸红色、蓝色区域，\n以测试 HitTestBehavior', style: TextStyle(color: Colors.grey),),
-              Stack(
-                children: [
-                  Listener(
-                    behavior: HitTestBehavior.deferToChild,
-                    onPointerDown: (event) {
-                      setState(() {
-                        ++_blueCount;
-                      });
-                    },
-                    child: ConstrainedBox(
-                      constraints: BoxConstraints.tight(const Size(300.0, 200.0)),
-                      child: const DecoratedBox(decoration: BoxDecoration(color: Colors.blue),),
-                    ),
-                  ),
-                  const IgnorePointer(
-                    ignoring: true,
-                    child: Padding(
-                      padding: EdgeInsets.fromLTRB(8, 0, 0, 0),
-                      child: Text('deferToChild'),
-                    ),
-                  ),
-                  Listener(
-                    behavior: HitTestBehavior.translucent,
-                    onPointerDown: (event) {
-                      setState(() {
-                        ++_redCount;
-                      });
-                    },
-                    child: SizedBox(
-                      width: 300,
-                      height: 200,
-                      child: Align(
-                        alignment: Alignment.center,
-                        child: Container(
-                          width: 200,
-                          height: 100,
-                          decoration: const BoxDecoration(color: Colors.red),
-                        ),
-                      ),
-                    ),
-                  ),
-                  const IgnorePointer(
-                    ignoring: true,
-                    child: Padding(
-                      padding: EdgeInsets.fromLTRB(58, 50, 0, 0),
-                      child: Text('translucent'),
-                    ),
-                  ),
-                ],
-              ),
-              Text('蓝色$_blueCount次        红色$_redCount次'),
-            ],
+      body: SafeArea(
+        child: SizedBox(
+          width: double.infinity,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text('请分别触摸红色、蓝色区域，以测试 HitTestBehavior',),
+                ),
+                const SizedBox(height: 32,),
+                ..._buildRegion(HitTestBehavior.deferToChild),
+                const Divider(height: 32,),
+                ..._buildRegion(HitTestBehavior.opaque),
+                const Divider(height: 32,),
+                ..._buildRegion(HitTestBehavior.translucent),
+                const SizedBox(height: 40,),
+              ],
+            ),
           ),
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _blue1Count = 0;
+            _red1Count = 0;
+            _blue2Count = 0;
+            _red2Count = 0;
+            _blue3Count = 0;
+            _red3Count = 0;
+          });
+        },
+        child: const Icon(Icons.refresh),
+      ),
     );
+  }
+
+  List<Widget> _buildRegion(HitTestBehavior behavior) {
+    int blueCount = 0;
+    int redCount = 0;
+    String behaviorName = '';
+    switch(behavior) {
+      case HitTestBehavior.deferToChild:
+        blueCount = _blue1Count;
+        redCount = _red1Count;
+        behaviorName = 'deferToChild';
+        break;
+      case HitTestBehavior.opaque:
+        blueCount = _blue2Count;
+        redCount = _red2Count;
+        behaviorName = 'opaque';
+        break;
+      case HitTestBehavior.translucent:
+        blueCount = _blue3Count;
+        redCount = _red3Count;
+        behaviorName = 'translucent';
+        break;
+    }
+
+    return <Widget>[
+      Stack(
+        children: [
+          Listener(
+            onPointerDown: (event) {
+              setState(() => _setCounter(behavior, Colors.blue));
+            },
+            child: ConstrainedBox(
+              constraints: BoxConstraints.tight(const Size(300.0, 200.0)),
+              child: const DecoratedBox(decoration: BoxDecoration(color: Colors.blue),),
+            ),
+          ),
+          Listener(
+            behavior: behavior,
+            onPointerDown: (event) {
+              setState(() => _setCounter(behavior, Colors.red));
+            },
+            child: SizedBox(
+              width: 300,
+              height: 200,
+              child: Align(
+                alignment: Alignment.center,
+                child: Container(
+                  width: 200,
+                  height: 100,
+                  decoration: const BoxDecoration(color: Colors.red),
+                ),
+              ),
+            ),
+          ),
+          IgnorePointer(
+            ignoring: true,
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(58, 50, 0, 0),
+              child: Text(behaviorName),
+            ),
+          ),
+        ],
+      ),
+      Text('蓝色$blueCount次        红色$redCount次'),
+    ];
+  }
+
+  void _setCounter(HitTestBehavior behavior, Color color) {
+    switch(behavior) {
+      case HitTestBehavior.deferToChild:
+        if(color == Colors.blue) {
+          ++_blue1Count;
+        } else if(color == Colors.red) {
+          ++ _red1Count;
+        }
+        break;
+      case HitTestBehavior.opaque:
+        if(color == Colors.blue) {
+          ++_blue2Count;
+        } else if(color == Colors.red) {
+          ++ _red2Count;
+        }
+        break;
+      case HitTestBehavior.translucent:
+        if(color == Colors.blue) {
+          ++_blue3Count;
+        } else if(color == Colors.red) {
+          ++ _red3Count;
+        }
+        break;
+    }
   }
 }
