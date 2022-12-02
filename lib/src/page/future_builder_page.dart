780fa345c3255e1c6b9caf143a9bd44f830046c2
conghaonet <hao.cong@app2m.com>
2022-12-02 16:37:52 +0800

    add future_builder_page.dart

--- lib/src/page/future_builder_page.dart
+++ lib/src/page/future_builder_page.dart
@@ -1,8 +1,39 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
-class FutureBuilderPage extends StatelessWidget {
+
+class FutureBuilderPage extends StatefulWidget {
   static const pagePathName = 'future_builder';
   const FutureBuilderPage({Key? key}) : super(key: key);
+
+  @override
+  State<FutureBuilderPage> createState() => _FutureBuilderPageState();
+}
+
+class _FutureBuilderPageState extends State<FutureBuilderPage> {
+  Future<String>? _future;
+
+  @override
+  void initState() {
+    super.initState();
+    /// 避免FutureBuilder重复刷新，此处是关键。
+    /// 将_future赋值给FutureBuilder.future，而不是_loadUrl函数。
+    _future = _loadUrl('https://flutter.dev');
+  }
+
+  Future<String> _loadUrl(String url) async {
+    debugPrint('==== start _loadUrl() ----');
+    await Future.delayed(const Duration(seconds: 2));
+    Future<HttpClientRequest> request = HttpClient().getUrl(Uri.parse(url));
+    Future<HttpClientResponse> response = request.then((value) {
+      return value.close();
+    });
+    return response.then((value) {
+      return value.transform(utf8.decoder).join();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,8 +41,52 @@ class FutureBuilderPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('防止FutureBuilder重复刷新'),
       ),
-      body: Container(
-
+      body: Column(
+        children: [
+          Row(
+            children: [
+              OutlinedButton(
+                child: const Text('setState'),
+                onPressed: () {
+                  setState(() {});
+                },
+              ),
+              OutlinedButton(
+                child: const Text('百度'),
+                onPressed: () {
+                  _future =  _loadUrl('https://wap.baidu.com');
+                  setState(() {});
+                },
+              ),
+              OutlinedButton(
+                child: const Text('头条'),
+                onPressed: () {
+                  _future =  _loadUrl('https://www.toutiao.com/');
+                  setState(() {});
+                },
+              ),
+            ],
+          ),
+          Container(height: 1, width: double.infinity, color: Colors.grey,),
+          Expanded(
+            child: FutureBuilder<String>(
+              future: _future, /// 使用Future对象，而非Future函数，可以避免重复刷新
+              initialData: '-= initial data =-',
+              builder: (context, snapshot) {
+                return Column(
+                  children: [
+                    Text(DateTime.now().toString()),
+                    Expanded(
+                      child: SingleChildScrollView(
+                        child: Text(snapshot.data ?? 'snapshot.data is null'),
+                      ),
+                    ),
+                  ],
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
