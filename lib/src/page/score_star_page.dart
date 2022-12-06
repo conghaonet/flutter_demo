70fdd559040296f9013defd447a7bcd1821ef8fd
conghaonet <hao.cong@app2m.com>
2022-12-07 02:09:58 +0800

    add GestureDetector

--- lib/src/page/score_star_page.dart
+++ lib/src/page/score_star_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_demo/src/widget/score_star.dart';
+import 'dart:math' as math;
 
 class ScoreStarPage extends StatefulWidget {
   static const pagePathName = 'score_star';
@@ -13,6 +14,17 @@ class ScoreStarPage extends StatefulWidget {
 class _ScoreStarPageState extends State<ScoreStarPage> {
   double? _height;
   double? _width;
+  double _score = 3.5;
+
+  void _changeScore(BuildContext ctx, Offset offset) {
+    Size size = ctx.size ?? Size.zero;
+    double offsetX = math.min(offset.dx, size.width);
+    offsetX = math.max(0, offsetX);
+    setState(() {
+      _score = double.parse(((offsetX / size.width) * 5).toStringAsFixed(1));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -60,12 +72,28 @@ class _ScoreStarPageState extends State<ScoreStarPage> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const Placeholder(child: SizedBox(height: 40, width: double.infinity,),),
+            Row(
+              children: const [
+                Expanded(child: Text('ABCDEFG')),
+                Expanded(
+                  child: ScoreStar(score: 3,),
+                ),
+              ],
+            ),
+            const SizedBox(height: 40,),
             Container(
               width: _width,
               height: _height,
-              color: Colors.blue,
-              child: const ScoreStar(score: 3.5,),
+              color: Colors.lightBlue,
+              child: Builder(builder: (ctx) {
+                return GestureDetector(
+                  child: ScoreStar(score: _score,),
+                  onTapDown: (details) => _changeScore(ctx, details.localPosition),
+                  onHorizontalDragUpdate: (details) => _changeScore(ctx, details.localPosition),
+                );
+              },),
             ),
+            const Text('点击或横向滑动以改变评分'),
           ],
         ),
       ),
